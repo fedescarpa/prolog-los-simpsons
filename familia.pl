6a7a792d8e59e269d993662d08b80bb476b5f538
Federico Scarpa <fedescarpa@gmail.com>
2018-06-24 17:25:48 -0300

    paso1

---
+++ familia.pl
@@ -0,0 +1,25 @@
+padreDe(abe, homero).
+padreDe(homero, bart).
+padreDe(homero, lisa).
+padreDe(homero, maggie).
+
+madreDe(jacqueline, marge).
+madreDe(marge, bart).
+madreDe(marge, lisa).
+madreDe(marge,maggie).
+
+progenitorDe(Progenitor, Hijo) :- 
+  padreDe(Progenitor, Hijo).
+
+progenitorDe(Progenitor, Hijo) :- 
+  madreDe(Progenitor, Hijo).
+
+hijoDe(Hijo, Progenitor) :- 
+  progenitorDe(Progenitor, Hijo).
+
+tieneHijo(Persona) :- 
+  progenitorDe(Persona, _).
+
+hermanos(Hermano1, Hermano2) :-
+  progenitorDe(Progenitor, Hermano1),
+  progenitorDe(Progenitor, Hermano2).
