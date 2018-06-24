23c0cb0b3c2e3c977c64132e8ebf2902f542d8f9
Federico Scarpa <fedescarpa@gmail.com>
2018-06-24 18:05:36 -0300

    agrego medio hermanos pero que anda mal

--- familia.pl
+++ familia.pl
@@ -34,6 +34,23 @@ tieneHijo(Persona) :-
   progenitorDe(Persona, _).
 
 hermanos(Hermano1, Hermano2) :-
-  progenitorDe(Progenitor, Hermano1),
-  progenitorDe(Progenitor, Hermano2),
+  compartenPadre(Hermano1, Hermano2),
+  compartenMadre(Hermano1, Hermano2).
+
+compartenPadre(Hermano1, Hermano2) :-
+  padreDe(Padre, Hermano1),
+  padreDe(Padre, Hermano2),
   Hermano1 \= Hermano2.
+
+compartenMadre(Hermano1, Hermano2) :-
+  madreDe(Madre, Hermano1),
+  madreDe(Madre, Hermano2),
+  Hermano1 \= Hermano2.
+
+medioHermanos(Hermano1, Hermano2) :-
+  not(compartenMadre(Hermano1, Hermano2)),
+  compartenPadre(Hermano1, Hermano2).
+
+medioHermanos(Hermano1, Hermano2) :-
+  not(compartenPadre(Hermano1, Hermano2)),
+  compartenMadre(Hermano1, Hermano2).
