af83b2e31a3f2f3dc9ab66d477b0c66c4cf537b8
Federico Scarpa <fedescarpa@gmail.com>
2018-06-24 19:10:24 -0300

    agrego las edades de los personajes

--- familia.pl
+++ familia.pl
@@ -54,3 +54,24 @@ medioHermanos(Hermano1, Hermano2) :-
 medioHermanos(Hermano1, Hermano2) :-
   compartenMadre(Hermano1, Hermano2),
   not(compartenPadre(Hermano1, Hermano2)).
+
+% Las edades de los simpson se calculan como si la fecha actual fuese (18 de diciembre de 1989)
+
+edad(abe, 62).         % 18 de Diciembre de 1927
+edad(edwina, 61).      % 15 de Marzo de 1928.
+edad(mona, 61).        % 14 de Octubre de 1928.
+edad(gaby, 57).        % 20 de Mayo de 1932.
+edad(homero, 33).      % 12 de Mayo de 1956.
+edad(abbie, 44).       % 13 de Noviembre de 1945.
+edad(herbert, 36).     % 22 de Agosto de 1953.
+edad(jacqueline, 61).  % 09 de Julio de 1928.
+edad(clancy, 62).      % 12 de Agosto de 1927.
+edad(marge, 34).       % 08 de Diciembre de 1955.
+edad(patty, 36).       % 02 de Febrero de 1953.
+edad(selma, 36).       % 02 de Febrero de 1953.
+edad(bart, 10).        % 01 de Abril de 1979.
+edad(hugo, 10).        % 01 de Abril de 1979.
+edad(lisa, 08).        % 09 de Mayo de 1981.
+edad(maggie, 01).      % 16 de Junio de 1988.
+edad(ling, 01).        % 03 de mayo de 1988.
+
