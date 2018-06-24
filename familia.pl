e428bd8440c1cf5974457c88cda3c9e3213551a8
Federico Scarpa <fedescarpa@gmail.com>
2018-06-24 17:53:02 -0300

    agrego mas info a la base de conocimiento

--- familia.pl
+++ familia.pl
@@ -1,12 +1,25 @@
+padreDe(abe, abbie).
 padreDe(abe, homero).
+padreDe(abe, herbert).
+padreDe(clancy, marge).
+padreDe(clancy, patty).
+padreDe(clancy, selma).
 padreDe(homero, bart).
+padreDe(homero, hugo).
 padreDe(homero, lisa).
 padreDe(homero, maggie).
 
+madreDe(edwina, abbie).
+madreDe(mona, homero).
+madreDe(gaby, herbert).
 madreDe(jacqueline, marge).
+madreDe(jacqueline, patty).
+madreDe(jacqueline, selma).
 madreDe(marge, bart).
+madreDe(marge, hugo).
 madreDe(marge, lisa).
 madreDe(marge,maggie).
+madreDe(selma, ling).
 
 progenitorDe(Progenitor, Hijo) :-
   padreDe(Progenitor, Hijo).
